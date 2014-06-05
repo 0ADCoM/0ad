f88a64b65cfff4816df0e620c7838b70075db695
leper <leper@wildfiregames.com>
2014-06-05 15:31:41 +0200

    Some TODOs.

--- binaries/data/mods/mod/gui/modmod/modmod.js
+++ binaries/data/mods/mod/gui/modmod/modmod.js
@@ -7,7 +7,14 @@ function init()
 	list.list_modSite = ["play0ad.com", "", ""];
 	list.list_modDescription = ["0 A.D. nuff said.", "A Chinese civ addon for 0 A.D.", "something"];
 	list.list_modTotalSize = ["123", "1", "0"];
-	list.list = ["0ad", "rote", "bla"]; // Use the mod folder name here
+	list.list = ["public", "rote", "bla"]; // Use the mod folder name here
+	// TODO rename public to 0ad?
+	// TODO filter the mod mod
+
+	// TODO Check savegame code to display nice mod names (after we have the logic for that)
+
+	// TODO Move this function out of the savegame code
+	//warn(uneval(Engine.GetEngineInfo()));
 }
 
 function modSelectionChanged()
